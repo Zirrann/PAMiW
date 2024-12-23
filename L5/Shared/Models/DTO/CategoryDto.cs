c3a0c5a420792ab4304fa5f074c6099c80d07bd4
Zirrann <140590309+Zirrann@users.noreply.github.com>
2024-12-23 10:29:11 +0100

    Remove L5

--- L5/Shared/Models/DTO/CategoryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Models.Dto
-{
-    public class CategoryDto
-    {
-        public int CategoryId { get; set; }
-        public string Name { get; set; }
-        // Pominięto produkty, aby uniknąć cyklu
-    }
-}
